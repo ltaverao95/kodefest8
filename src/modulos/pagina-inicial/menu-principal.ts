9f9b1d423c70f6e812b9c64087fe3f50f776b198
Luis Felipe Tavera Orozco <lftavera@hotmail.com>
2017-08-12 13:02:05 -0500

    refactorización de código

--- src/modulos/pagina-inicial/menu-principal.ts
+++ src/modulos/pagina-inicial/menu-principal.ts
@@ -19,51 +19,31 @@ export namespace MenuPrincipal {
     export namespace Metodos {
 
         export const sendMessage = (msg: Message) => {
-            Data.Chats.guardarContexto(msg,  Contextos.PaginaInicial.menuPrincipal).then(() => {
-                    const messageOptions = {
-                        reply_markup: {
-                            one_time_keyboard:true,
-                            keyboard: [
-                                [
-                                    {
-                                        text: Comandos.PaginaInicial.MenuPrincipal.consultas,
-                                        //callback_data: Opciones.Consultas
-                                    }
-                                ],
-                                [
-                                    {
-                                        text: Comandos.PaginaInicial.MenuPrincipal.facturas,
-                                        //callback_data: Opciones.Facturas
-                                    }
-                                ],
-                                [
-                                    {
-                                        text: Comandos.PaginaInicial.MenuPrincipal.transferencias,
-                                        //callback_data: Opciones.Transferencias,
-                                    }
-                                ],
-                                [
-                                    {
-                                        text: Comandos.PaginaInicial.MenuPrincipal.configuracion,
-                                        //callback_data: Opciones.Configuración,
-                                    }
-                                ]
-                            ],
-                        } as ReplyKeyboardMarkup
-                    } as SendMessageOptions;
+            Data.Chats.guardarContexto(msg, Contextos.PaginaInicial.menuPrincipal).then(() => {
+                const messageOptions = {
+                    reply_markup: {
+                        one_time_keyboard: true,
+                        keyboard: [
+                            [{ text: Comandos.PaginaInicial.MenuPrincipal.consultas }],
+                            [{ text: Comandos.PaginaInicial.MenuPrincipal.facturas }],
+                            [{ text: Comandos.PaginaInicial.MenuPrincipal.transferencias }],
+                            [{ text: Comandos.PaginaInicial.MenuPrincipal.configuracion }]
+                        ],
+                    } as ReplyKeyboardMarkup
+                } as SendMessageOptions;
 
-                    bot.sendMessage(
-                        msg.chat.id,
-                        `¿Qué quieres hacer?`,
-                        messageOptions
-                    );
-                });
+                bot.sendMessage(
+                    msg.chat.id,
+                    `¿Qué quieres hacer?`,
+                    messageOptions
+                );
+            });
         }
 
-        export const onMenuPrincipal = (msg: Message) => { 
+        export const onMenuPrincipal = (msg: Message) => {
             sendMessage(msg);
         }
-    }    
+    }
 
     export namespace eventHandlers {
 
