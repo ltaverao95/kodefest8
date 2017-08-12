19a2b57547fdbd59a21e2ce75a56e95d7426c8a4
joseucarvajal <joseucarvajal@gmail.com>
2017-08-11 22:56:39 -0500

    Ignorando archivos

--- dist/core/constants.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-//export const Status = Constants.Chat.Status;
-//export const Commands = Constants.Chat.Command; 
--- dist/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-require('./initDatabase');
-require('./initBot');
-require('./app/index');
