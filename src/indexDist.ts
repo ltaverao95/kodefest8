4aa177dc4a794fe54b3ede724a710b59c609bd9e
Luis Felipe Tavera Orozco <lftavera@hotmail.com>
2017-08-12 11:12:18 -0500

    se incluyeron archivos para manejo de despliegue en producción

---
+++ src/indexDist.ts
@@ -0,0 +1,16 @@
+/**
+ * Infraestructura
+ */
+require('./initDatabase');
+require('./initBotDist');
+
+/**
+ * Módulos
+ */
+require('./modulos/pagina-inicial');
+require('./modulos/consultas');
+require('./modulos/facturas');
+require('./modulos/transferencias');
+require('./modulos/configuracion');
+
+require('./modulos/index');
---
+++ src/initBotDist.ts
@@ -0,0 +1,21 @@
+import {
+    TelegramBot as TelegramBotModel
+  } from './bot/TelegramBot';
+  
+  const TelegramBot = require('node-telegram-bot-api');
+  
+  // replace the value below with the Telegram token you receive from @BotFather
+  const token = '436331165:AAGcARHfHuuSSaJ_e1iAOx2q_0lrxpWoYYM';
+  
+  // Create a bot that uses 'polling' to fetch new updates
+  const prodOptions = {
+    webHook: {
+      port: process.env.PORT || 5000
+    }
+  };
+  
+  export const bot:TelegramBotModel = new TelegramBot(token, prodOptions);
+  
+  const url = 'https://kodefest8.herokuapp.com/dist/index.js';
+  
+  bot.setWebHook(`${url}/bot${token}`);
