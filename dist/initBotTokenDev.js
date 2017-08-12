b5bda1107ab673eddd96a0e7909c172d8b480002
joseucarvajal <joseucarvajal@gmail.com>
2017-08-12 15:41:09 -0500

    delete dist

--- dist/initBotTokenDev.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.token = '447133612:AAG96SqODQfDB9sXv9YB9GLdWEg15BxekPQ';
--- dist/initBotTokenDist.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.token = '436331165:AAGcARHfHuuSSaJ_e1iAOx2q_0lrxpWoYYM';
