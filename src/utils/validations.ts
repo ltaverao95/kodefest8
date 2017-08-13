617d5258dcae18209d6d4ccd2715d5fce3642c83
joseucarvajal <joseucarvajal@gmail.com>
2017-08-13 07:19:06 -0500

    Generación de GUID

--- src/utils/validations.ts
+++ src/utils/validations.ts
@@ -41,4 +41,11 @@ export namespace Validaciones {
 
         return true;
     }
+
+    export const generarGUID = () => {
+        return 'xx-xxxxxxxxxx'.replace(/[x]/g, function (c) {
+            var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+            return v.toString(16);
+        });
+    }
 }
